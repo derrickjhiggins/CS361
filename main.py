fd597a1ed6937281081f98f71069ea7216f91632
derrickjhiggins <derrick.higgins@outlook.com>
2024-02-23 13:23:03 -0600

    - refactor main function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from generate import generate
-from openai import OpenAI
 import os
+import sys
 from dotenv import load_dotenv
+
+from openai import OpenAI
+from generate import generate
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,16 +11,11 @@ load_dotenv()
 # establish openai api client connection
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def main():
-    # print introduction screen
-    with open('startup.txt', 'r') as startup:
-        for line in startup.readlines():
-            print(line.strip())
-
+def getFirstInput():
     # get first user input
+    filepath = None
     user_input = input('||  ')
     if user_input == '1':
-        # print instructions
         with open('instruction_files/generate_new_icebreaker.txt', 'r') as instructions:
             for line in instructions.readlines():
                 print(line.strip())
@@ -28,12 +25,38 @@ def main():
             filepath = input('||    ')
             for line in instructions.readlines():
                 print(line.strip())
+    elif user_input == '3':
+        with open('instruction_files/settings.txt', 'r') as settings:
+            for line in settings.readlines():
+                print(line.strip())
+            sys.exit()
+    elif user_input == chr(27):
+        print("|| Goodbye for now!")
+        sys.exit()
 
+    return filepath
+
+def getSecondInput(filepath):
     # get second user input
     user_input = input('||  ')
     if user_input == '1':
         print(f"||  Here's a good icebreaker: \n")
-        generate(client)
+        generate(client, filepath)
+    elif user_input == chr(27):
+        print("|| Goodbye for now!")
+        sys.exit()
+    else:
+        additionalDirections = input('|| ')
+        generate(client, filepath, additionalDirections)
+
+def main():
+    # print introduction screen
+    with open('instruction_files/startup.txt', 'r') as startup:
+        for line in startup.readlines():
+            print(line.strip())
+
+    filepath = getFirstInput()
+    getSecondInput(filepath)
 
 if __name__ == "__main__":
     main()
